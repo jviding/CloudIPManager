597f234c81f70924337f20fa46500966c96ff17a
jviding <jasu.viding@helsinki.fi>
2016-02-25 21:13:54 +0300

    .

--- routes.js
+++ routes.js
@@ -72,8 +72,11 @@ module.exports = function(apiRoutes, app, jwt, callback) {
 	// route to get external IP address of raspberry
 	apiRoutes.get('/getip', function (req, res) {
 		IP.findOne({ owner: req.body.name }, function (err, ipAddr) {
-			res.json({lol:true});
-			//res.json({ RaspBerry: ipAddr.raspName, ip: ipAddr.ip, lastUpdate: ipAddr.lastUpdate });
+			/*res.json({ 
+				name:       ipAddr.raspName, 
+				ip:         ipAddr.ip, 
+				lastUpdate: ipAddr.lastUpdate });*/
+			console.log(ipAddr);
 		});
 	});
 
